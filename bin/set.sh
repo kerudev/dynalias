2af1f125985147ed735e728de912437b745423f3
Cristian Marcos Martin <cristianmarcosmartin1@gmail.com>
2025-07-28 23:47:34 +0200

    Fixed the bug where function aliases wouldn't be ran because they lacked the execute permission

--- bin/set.sh
+++ bin/set.sh
@@ -3,24 +3,28 @@
 # set.sh - Reads the alias files and formats them into lines to update the output file.
 # Parameters:
 #   $1 - Type of alias (text or func)
+#   $2 - Name of alias
 
 source "$DYNALIAS_LIB/alias.sh"
 source "$DYNALIAS_LIB/output.sh"
 
 if [[ -n "$1" ]]; then
-    if [[ "$1" == "text" || "$1" == "func" ]]; then
-        cd "$DYNALIAS_ALIAS/$1"
-        nano || exit
-        cd "$DYNALIAS_ROOT"
-    else
+    if [[ "$1" != "text" && "$1" != "func" ]]; then
         err 1 "unknown alias type; must be 'text' or 'func'"
     fi
+
+    if [[ -z "$2" ]]; then
+        err 1 "please provide an alias name"
+    fi
+
+    alias_path="$DYNALIAS_ALIAS/$1/$2"
+    nano "$alias_path" || vi "$alias_path" || throw "it was not possible to open a text editor"
+    chmod +x "$alias_path"
 fi
 
 check_output
 
-for dir in "$DYNALIAS_ALIAS"/*
-do
+for dir in "$DYNALIAS_ALIAS"/*; do
     if [[ -z "$(ls -A "$dir")" ]]; then
         err 0 "$dir is empty; can't create aliases from this directory"
         continue
@@ -30,8 +34,7 @@ do
 
     write_output "# $type"
 
-    for file in "$dir"/*
-    do  
+    for file in "$dir"/*; do  
         write_output "$(format_alias "$type" "$file")"
     done
 
